d0e5bd107d36c2732289b219a246180f971f1532
yudhapaderip97 <yudha@visiglobalteknologi.co.id>
2023-12-27 15:16:05 +0700

    feat/stts-card: edited margin logo

--- src/components/layout/monitors/monitor1.tsx
+++ src/components/layout/monitors/monitor1.tsx
@@ -1,43 +1,8 @@
 import type { FunctionComponent } from "@/src/common/types";
 import { StatusCard } from "../../cards";
 import Typography from "../../ui/default-typography";
-import { MyMap } from "../../ui/default-map";
-// import logger from "../../../libs/logger";
-import { countStatus } from "../../../utils/arrayUtils";
-import { useEffect, useState } from "react";
-import { IWimStatusResponse } from "@/src/types/response";
-import Skeleton from "../../ui/default-skeleton";
-// import { IWimStatusResponse } from "@/src/types/response";
 
-interface MonitorTopLeftProps {
-	selectedSiteMapHook: React.Dispatch<
-		React.SetStateAction<IWimStatusResponse | null>
-	>;
-	listMergedSiteData: any[];
-	isLoading: boolean;
-}
-
-const MonitorTopLeft = ({
-	selectedSiteMapHook,
-	listMergedSiteData,
-	isLoading,
-}: MonitorTopLeftProps): FunctionComponent => {
-	const [countOk, setCountOk] = useState<number | string>(0);
-	const [countWarning, setCountWarning] = useState<number | string>(0);
-	const [countOff, setCountOff] = useState<number | string>(0);
-
-	useEffect(() => {
-		if (listMergedSiteData) {
-			setCountOff(countStatus(listMergedSiteData, "red"));
-			setCountOk(countStatus(listMergedSiteData, "green"));
-			setCountWarning(countStatus(listMergedSiteData, "orange"));
-		} else {
-			setCountOff("error");
-			setCountOk("error");
-			setCountWarning("error");
-		}
-	}, [listMergedSiteData]);
-
+const MonitorTopLeft = (): FunctionComponent => {
 	return (
 		<>
 			{/* Monitor 1 */}
@@ -47,7 +12,7 @@ const MonitorTopLeft = ({
 					<img
 						src="../public/Logo-GBU-2.svg"
 						alt="Logo-GBU-2"
-						className="object-cover ml-9 w-40 h-20"
+						className="object-cover ml-10 w-60 h-20"
 					/>
 				</div>
 				<div className="w-full text-center col-span-2">
@@ -55,21 +20,16 @@ const MonitorTopLeft = ({
 						WIM Monitoring
 					</Typography>
 				</div>
-				<div className="w-full text-center col-span-1 border-l-2 border-gray-500">
-
+				<div className="w-full text-center col-span-1 border-l-2 border-gray-300">
 					<img
 						src="../public/Logo-VGT-1.svg"
 						alt="Logo-VGT-1"
-						className="ml-9 object-cover w-44 h-20"
+						className="object-cover ml-14 w-48 h-20"
 					/>
 				</div>
 				{/* map */}
-				<div className="col-span-4 row-span-4 text-center h-full">
-					<MyMap
-						isLoading={isLoading}
-						markers={listMergedSiteData}
-						selectedSiteMapHook={selectedSiteMapHook}
-					/>
+				<div className="col-span-4 row-span-4 bg-slate-900 text-amber-50 text-center h-full">
+					map
 				</div>
 				{/* ok status */}
 				<div className="row-span-2 shadow-md m-2 rounded-lg">
@@ -81,13 +41,9 @@ const MonitorTopLeft = ({
 							OK
 						</Typography>
 						<div className="border-t border-gray-500 flex-grow">
-							{isLoading ? (
-								<Skeleton className="h-full w-full" />
-							) : (
-								<Typography variant="j1" className="text-center text-7xl">
-									{countOk}
-								</Typography>
-							)}
+							<Typography variant="j1" className="text-center text-7xl">
+								15
+							</Typography>
 						</div>
 					</StatusCard>
 				</div>
@@ -101,13 +57,9 @@ const MonitorTopLeft = ({
 							WARNING
 						</Typography>
 						<div className="border-t border-gray-500 flex-grow">
-							{isLoading ? (
-								<Skeleton className="h-full w-full" />
-							) : (
-								<Typography variant="j1" className="text-center text-7xl">
-									{countWarning}
-								</Typography>
-							)}
+							<Typography variant="j1" className="text-center text-7xl">
+								0
+							</Typography>
 						</div>
 					</StatusCard>
 				</div>
@@ -121,13 +73,9 @@ const MonitorTopLeft = ({
 							OFF
 						</Typography>
 						<div className="border-t border-gray-500 flex-grow">
-							{isLoading ? (
-								<Skeleton className="h-full w-full" />
-							) : (
-								<Typography variant="j1" className="text-center text-7xl">
-									{countOff}
-								</Typography>
-							)}
+							<Typography variant="j1" className="text-center text-7xl">
+								0
+							</Typography>
 						</div>
 					</StatusCard>
 				</div>
